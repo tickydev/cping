85dbb83fd82be5c8aaf98f5ac30e24615c267f4e
tickydev <110549027+tickydev@users.noreply.github.com>
2022-08-11 22:58:14 +0300

    Update main (source).cpp

--- main (source).cpp
+++ main (source).cpp
@@ -9,12 +9,12 @@ int main(int argc, char const* argv[])
     float b = 1000;
     float a = 999;
 
-    print("& github: github.com/tickydev/cping/");
+    cout << "& github: github.com/tickydev/cping/";
     system("title Console");
 
     if (b > a)
     {
-        system("ping example.com /n 4/l 40 /w 15");
+        system("ping example.com /n 4 /l 40 /w 15");
     }
 
 
